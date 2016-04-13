1e8348b754886d975ea2967773a76f2fd5908a46
jamesmitchell81 <james.mitchell81@live.co.uk>
2016-04-13 17:08:17 +0100

    Possible form for template classes

---
+++ ImageCropUI/h.h
@@ -0,0 +1,18 @@
+//
+//  h.h
+//  ImageCropUI
+//
+//  Created by James Mitchell on 13/04/2016.
+//  Copyright © 2016 James Mitchell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface h : NSObject
+{
+    NSBezierPath* path;
+}
+
+@property (nonatomic) NSBezierPath* path;
+
+@end
