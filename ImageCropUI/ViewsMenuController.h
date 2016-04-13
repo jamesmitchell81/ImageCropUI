13c35a14fec3a63a9eaf0406a94b07efa166dba8
jamesmitchell81 <james.mitchell81@live.co.uk>
2016-04-13 17:07:37 +0100

    Added View tab to Menu Bar allow for switching views

---
+++ ImageCropUI/ViewsMenuController.h
@@ -0,0 +1,19 @@
+//
+//  ViewsMenuController.h
+//  ImageCropUI
+//
+//  Created by James Mitchell on 13/04/2016.
+//  Copyright © 2016 James Mitchell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewsMenuController : NSMenu
+{
+    
+}
+
+- (IBAction) displayDropZoneView:(id)sender;
+- (IBAction) displayTemplateView:(id)sender;
+
+@end
